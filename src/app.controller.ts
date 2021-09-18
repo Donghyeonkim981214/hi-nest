71aeb48fd8efbc3a7e804a341c64f8e3fca8e7d2
Donghyeonkim981214 <donghyeon.kim.121498@gmail.com>
2021-09-19 07:49:26 +0900

    #1.2 Services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,8 +11,15 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  //데코레이터의 경우 함수나 클래스에 붙여있어야 한다.
   @Get('/hello')
   sayHello(): string {
-    return 'Hello, everyone!';
+    //NestJS는 controller와 service의 함수명이 같기를 권장하지만 그렇지 않다고 실행안되는 것은 아니다.
+    return this.appService.getHi();
   }
 }
+
+
+//NestJS는 controller를 비즈니스 로직이랑 구분짓기를 원함
+//그래서 controller의 역할은 url을 가져오고 function을 실행하는 것 까지이다.
+//나머지 비즈니스 로직은 service에서 설계한다.
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello NestJS!';
   }
+
+  getHi(): string {
+    return 'Hi NestJS!';
+  }
 }
