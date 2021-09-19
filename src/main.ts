0db7b587070007eecc4055bc6cadf39c747b1b99
Donghyeonkim981214 <donghyeon.kim.121498@gmail.com>
2021-09-19 15:12:47 +0900

    #2.4 DTOs and Validation part One

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   //await NestFactory.create(AppModule)을 호출한다.
   const app = await NestFactory.create(AppModule);
+  //유효성 검사를 위한 파이프(파이프는 미들웨어와 비슷하다고 보면)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whiteList -> 엔티티 데코레이터에 없는 프로퍼티 값은 무조건 거름
+      whitelist: true,
+      // forbidNonWhitelisted -> 엔티티 데코레이터에 없는 값 인입시 그 값에 대한 에러메세지 알려줌
+      forbidNonWhitelisted: true,
+      //원래 param으로 들어오는 값은 무조건 string으로 들어온다. transform은 컨트롤러에서 id로 받는 값에 타입을 지정했다면 nest에서 자체적으로 타입을 변환하게다는 옵션
+      transform: true,
+    }),
+  );
+
   //위에서 호출한 어플리케이션을 3000번의 포트로 리스닝한다.
   await app.listen(3000);
 }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,11 +10,13 @@ import {
   } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 //아래의 'movies' 덕분에 해당 모듈로 접속하기 위한 url은 주소/movies가 된다. 물론 변경가능하며 지울 수도 있다.
 @Controller('movies')
 export class MoviesController {
 
+  //NestJS가 controller에서 service에 접근하는 방법
   constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
@@ -24,27 +26,29 @@ export class MoviesController {
 
   //url에 있는 id parameter를 사용하기 위해 @param을 사용한다. https://docs.nestjs.com/openapi/types-and-parameters#types-and-parameters
   //그리고 이를 사용하기 위해 @Get('/:id')의 id와 @Param('id')의 id처럼 변수명이 동일해야 한다. 그리고 실제 함수내에서 쓸 변수명 movieId는 변수명이 달라도 괜찮다.
+  //원래 id는 string이지만 main.ts 에서 transform을 true로 하여 id를 number형태로 형변환 시켜준다.
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData) {
     return {
       updatedMovie: movieId,
       ...updateData,
     };
+  }
 }
 
-//express.js에서와는 달리 json을 사용하기 위한 추가 설정이 필요 없다.
+/* express.js에서와는 달리 json을 사용하기 위한 추가 설정이 필요 없다. */
