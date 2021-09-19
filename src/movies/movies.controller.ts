e8aae95e746e08447be98a974923736b36591290
Donghyeonkim981214 <donghyeon.kim.121498@gmail.com>
2021-09-19 11:24:48 +0900

    #2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,35 +8,35 @@ import {
     Body,
     Query,
   } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 //아래의 'movies' 덕분에 해당 모듈로 접속하기 위한 url은 주소/movies가 된다. 물론 변경가능하며 지울 수도 있다.
 @Controller('movies')
 export class MoviesController {
-  @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
 
-  @Get('search')
-  search(@Query('year') seachingYear: string) {
-    return `We are searching for a movie made after: ${seachingYear}`;
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   //url에 있는 id parameter를 사용하기 위해 @param을 사용한다. https://docs.nestjs.com/openapi/types-and-parameters#types-and-parameters
   //그리고 이를 사용하기 위해 @Get('/:id')의 id와 @Param('id')의 id처럼 변수명이 동일해야 한다. 그리고 실제 함수내에서 쓸 변수명 movieId는 변수명이 달라도 괜찮다.
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
